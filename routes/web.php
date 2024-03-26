870966987af695f94d4ae16763b4ba830fff71e9
AREZA13 <arezasoft@icloud.com>
2024-03-26 13:22:47 +0300

    Implemented routes for get one card, stop game and start over methods

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/start-game-page', [\App\Http\Controllers\GameController::class, 'ho
 Route::get('/game', [\App\Http\Controllers\GameController::class, 'index']
 )->name('get-two-cards-game-page');
 
-Route::get('/gameDelete', [\App\Http\Controllers\GameController::class, 'removeSession']
-)->name('game-delete');
+Route::get('/more-card', [\App\Http\Controllers\GameController::class, 'oneMoreCardPage']
+)->name('one-more-card-page');
 
+Route::get('/stop-game', [\App\Http\Controllers\GameController::class, 'generateRandomDealerScore'])
+->name('stop-game-page');
+
+Route::get('/game-delete', [\App\Http\Controllers\GameController::class, 'removeSession']
+)->name('game-delete');
