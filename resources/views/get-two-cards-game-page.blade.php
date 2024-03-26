d39a7845d22073f992af6231d55040e2b101eee3
AREZA13 <arezasoft@icloud.com>
2024-03-26 13:20:09 +0300

    made buttons for end game and get one more card

--- resources/views/get-two-cards-game-page.blade.php
+++ resources/views/get-two-cards-game-page.blade.php
@@ -4,6 +4,9 @@
 var_dump($card->getAsString())
 }}
 @endforeach
+<br>
+<a href="{{route('one-more-card-page')}}"><button type="button" class="btn btn-primary">One more card</button></a><br>
+<a href="{{route('stop-game-page')}}"><button type="button" class="btn btn-primary">Stop</button></a>
 </body>
 <footer>
     @yield('footer')
