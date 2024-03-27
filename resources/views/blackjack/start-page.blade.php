d79b47a3865f0907660cff04a6ed00fb3802a943
AREZA13 <arezasoft@icloud.com>
2024-03-27 17:40:16 +0300

    Added buttons for / page. Added "switch game button"

--- resources/views/blackjack/start-page.blade.php
+++ resources/views/blackjack/start-page.blade.php
@@ -3,5 +3,8 @@
     <a href="{{ route('blackjack-get-two-cards-game-page') }}">
         <button type="button" class="btn btn-primary">Start game</button>
     </a>
+    <a href="{{ route('choose-game') }}">
+        <button type="button" class="btn btn-dark">Switch game</button>
+    </a>
 
 @endsection
--- resources/views/choose-game.blade.php
+++ resources/views/choose-game.blade.php
@@ -1,9 +1,13 @@
 @extends('blackjack/template')
 @section('content')
     <div style="text-align: center;">
-   <br>
-   <a href="{{ route('blackjack-game-delete') }}">
-       <button type="button" class="btn btn-warning">Start Over</button>
-   </a>
+
+        <a href="{{ route('blackjack-start-game-page') }}">
+            <button type="button" class="btn btn-warning">Play BlackJack</button>
+        </a>
+
+        <a href="{{ route('blackjack-game-delete') }}">
+            <button type="button" class="btn btn-secondary">Play Texas Holdem</button>
+        </a>
     </div>
 @endsection
