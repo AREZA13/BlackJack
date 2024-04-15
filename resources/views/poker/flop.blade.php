5f6c47f11bdc92fedd0318aab1105aa5bc0b1333
AREZA13 <arezasoft@icloud.com>
2024-04-15 18:03:09 +0300

    Front end in flop blade. Added new buttons with routing.

--- resources/views/poker/flop.blade.php
+++ resources/views/poker/flop.blade.php
@@ -1,6 +1,11 @@
+@php
+    use App\Game\Poker\Player;
+ use App\Game\Poker\Poker;
+@endphp
 <?php
-/** @var \App\Game\Poker\Player[] $players */
+/** @var Player[] $players */
 /** @var int $pot */
+/** @var array $tableCards */
 ?>
 @extends('poker/template')
 @section('content')
@@ -13,8 +18,10 @@
         }
     </style>
 
-
-
+    <div style="text-align: center;">
+        <button type="button" class="btn btn-primary btn-lg" disabled>Total Pot {{$pot}}</button>
+    </div>
+    <br>
     <div style="text-align: center;">
         @foreach($players as $player)
             <div>
@@ -23,25 +30,47 @@
                 @endforeach
             </div>
         @endforeach
-    </div>
-    <form class="form-inline" action="{{ route('preFlopBet') }}" method="POST">
-        @csrf
-        <div class="form-group mx-sm-3 mb-2">
-            <br>
-            <input type="number" name="bet" class="form-control" max="{{ $players[0]->getStack() }}" placeholder="Type Your bet">
-            <button type="submit" class="btn btn-success mb-2">Bet</button>
-        </div>
-    </form>
-    <div class="form-group mx-sm-3 mb-2">
-        <form class="form-inline" action="{{ route('preFlopBet') }}" method="POST">
-            @csrf
-            <input hidden="hidden" type="number" name="bet" class="form-control" value="{{ $players[0]->getStack() }}">
-            <button type="submit" class="btn btn-warning mb-2">All in</button>
-        </form>
+        <br>
+        @foreach($tableCards as $tableCard)
+
+                <img src="{{ $tableCard->getAsImagePath() }}" alt="{{ $tableCard->getAsString() }}">
+        @endforeach
     </div>
     <div style="text-align: center;">
-        <button type="submit" class="btn btn-secondary mb-2">Check</button>
-        <button type="submit" class="btn btn-danger mb-2">Fall</button>
-    </div>
+        <form class="form-inline" action="{{ route('flopBet') }}" method="POST">
+            @csrf
+            <input type="number" name="bet" class="form-control" min="1" max="{{ $players[0]->getStack() }}"
+                   placeholder="Type Your bet">
+            <button type="submit" class="btn btn-success mb-2">Bet</button>
+        </form>
+        <br>
+        <form style="display: inline;" class="form-inline" action="{{ route('flopBet') }}" method="POST">
+            @csrf
+            <input hidden="hidden" type="number" name="bet" class="form-control" value="{{ $players[0]->getStack() }}">
+            <button type="submit" class="btn btn-info mb-2">All in</button>
+        </form>
 
+        <form style="display: inline;" class="form-inline" action="{{ route('flopBet') }}" method="POST">
+            @csrf
+            <input hidden="hidden" type="number" name="bet" class="form-control" value="0">
+            <button type="submit" class="btn btn-secondary mb-2">Check</button>
+        </form>
+
+        <form style="display: inline;" class="form-inline" action="{{ route('poker-game-delete') }}">
+            @csrf
+            <button type="submit" class="btn btn-danger mb-2">Fall</button>
+        </form>
+        <br>
+        <a href="{{ route('turn') }}">
+            <button type="button" class="btn btn-primary btn-lg">Get turn</button>
+        </a>
+    </div>
+    <br>
+    <br>
+    <a href="{{ route('poker-game-delete') }}">
+        <button type="button" class="btn btn-warning">Start Over</button>
+    </a>
+    <a href="{{ route('choose-game') }}">
+        <button type="button" class="btn btn-dark">Switch game</button>
+    </a>
 @endsection
