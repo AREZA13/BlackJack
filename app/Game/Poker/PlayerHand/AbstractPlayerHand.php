d939f19caaab228af8eae84861df6077b8a85ba9
AREZA13 <arezasoft@icloud.com>
2024-04-25 14:59:45 +0300

    Added methods getCards ( 5 cards) and compare function now has array of 7

--- app/Game/Poker/PlayerHand/AbstractPlayerHand.php
+++ app/Game/Poker/PlayerHand/AbstractPlayerHand.php
@@ -11,11 +11,23 @@ abstract class AbstractPlayerHand
      * @param array{0: Card, 1: Card, 2: Card, 3: Card, 4: Card} $cards
      */
     public function __construct(
-        protected array $cards,
+        protected array     $cards,
         public readonly int $playerId,
     )
     {
     }
 
+    /**
+     * @return Card[]
+     */
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+
+
     abstract public function compare(AbstractPlayerHand $otherHand): CompareHandResultEnum;
+    /**
+     * @param array{0: Card, 1: Card, 2: Card, 3: Card, 4: Card, 5: Card, 6: Card} $arrayOfSevenCards
+     */
 }
