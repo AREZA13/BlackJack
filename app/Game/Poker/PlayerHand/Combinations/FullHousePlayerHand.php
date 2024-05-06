c28546bd41cc73eb59d9f6fd160125a577be08ff
AREZA13 <arezasoft@icloud.com>
2024-05-06 14:13:45 +0300

    added logics into FullHousePlayerHand.php

--- app/Game/Poker/PlayerHand/Combinations/FullHousePlayerHand.php
+++ app/Game/Poker/PlayerHand/Combinations/FullHousePlayerHand.php
@@ -7,6 +7,28 @@ use App\Game\Poker\PlayerHand\AbstractPlayerHand;
 
 class FullHousePlayerHand extends AbstractPlayerHand
 {
+    public function __construct(
+        private readonly array $threeCards,
+        private readonly array $twoCards,
+        int                    $playerId,
+    )
+    {
+        parent::__construct(
+            array_merge($threeCards, $twoCards),
+            $playerId
+        );
+    }
+
+    public function getThreeCards(): array
+    {
+        return $this->threeCards;
+    }
+
+    public function getTwoCards(): array
+    {
+        return $this->twoCards;
+    }
+
     public function compare(AbstractPlayerHand $otherHand): CompareHandResultEnum
     {
         if ($otherHand instanceof self) {
