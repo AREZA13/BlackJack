c2ae7243bd5582b271e425820f27912fbbe2b38a
AREZA13 <arezasoft@icloud.com>
2024-05-13 17:51:28 +0300

    refactored - added constructor

--- app/Game/Poker/PlayerHand/Combinations/OnePairPlayerHand.php
+++ app/Game/Poker/PlayerHand/Combinations/OnePairPlayerHand.php
@@ -7,6 +7,28 @@ use App\Game\Poker\PlayerHand\AbstractPlayerHand;
 
 class OnePairPlayerHand extends AbstractPlayerHand
 {
+    public function __construct(
+        private readonly array $pair,
+        private readonly array $threeCards,
+        int                    $playerId,
+    )
+    {
+        parent::__construct(
+            array_merge($pair, $threeCards),
+            $playerId
+        );
+    }
+
+    public function getPair(): array
+    {
+        return $this->pair;
+    }
+
+    public function getThreeCards(): array
+    {
+        return $this->threeCards;
+    }
+
     public function compare(AbstractPlayerHand $otherHand): CompareHandResultEnum
     {
         if ($otherHand instanceof self) {
